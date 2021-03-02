5a6bd0dfc6484ce360753ec3d200dc022111a204
sunyue216 <2184681342@qq.com>
2021-03-02 19:45:50 +0800

    完成前端的登录注册县官的界面

---
+++ book-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,10 @@
+import { defineComponent } from 'vue';
+import { UserOutlined } from '@ant-design/icons-vue';
+export default defineComponent({
+    components: {
+      UserOutlined,
+    },
+    setup() {
+
+    },
+});
